19e7d18d84cad6aac9e07ee83f1852f15da99963
silvermistshadow <bmead72@gmail.com>
2020-04-15 11:18:25 -0700

    separate into two controllers

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      return View();
     }
 
   }
